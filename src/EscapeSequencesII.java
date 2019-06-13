2b093cdf58de47f4f6720ba42df77b8ef3b3929f
GBTC441002UR <GBTC441002UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-06-13 13:52:24 -0400

    first commit

---
+++ src/EscapeSequencesII.java
@@ -0,0 +1,9 @@
+public class EscapeSequencesII {
+
+    public static void main(String[]args){
+
+        System.out.println("Cost" + "\t" + "Quantity" + "\t" +"Total");
+        System.out.println("$1,000.00"+"\t"+"4"+"\t"+"\t"+"$4,000.00");
+        System.out.println("$"+"\t"+"50.00"+"\t"+"8"+"\t"+"\t"+"$400.00");
+    }
+}
